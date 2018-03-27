a02f31d9fc5034108d07d032db1697bfff6ced78
bartwiharto <bartwiharto@gmail.com>
2018-03-27 17:54:02 -0600

    Commit #1 - Beginning stages of html and js build

---
+++ app.js
@@ -0,0 +1,9 @@
+const userForm = document.getElementById("user-Form");
+
+  userForm.addEventListener("submit", function(event){
+    event.preventDefault();
+
+    let userInput = document.getElementById('user-Input').value;
+});
+
+console.log(userInput);
