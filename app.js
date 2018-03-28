83ec8a399e0371c2b3c69ddc85b24d3134b5c52d
bartwiharto <bartwiharto@gmail.com>
2018-03-27 18:50:38 -0600

    Commit #4 - Able to input message and create a paragraph in the body

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const userForm = document.getElementById("user-Form");
 
-console.log(userForm);
-
 	userForm.addEventListener('submit', function(event) {
     	event.preventDefault();
 	const userInput = document.getElementById("user-Input").value;
 	const submit = document.getElementById("submitButton").value;
 
-console.log(userInput);
+	let userMessage = document.createElement('P');
+	let t  = document.createTextNode(userInput);
+	userMessage.appendChild(t);
+    document.body.appendChild(userMessage);
+
 });
 
 // console.log(userInput);
