6faae30d6db1246f1f4119d2357a86b835002bb0
bartwiharto <bartwiharto@gmail.com>
2018-04-01 16:14:28 -0600

    finished with VERY minimal style. Can't add w/o messing up JS code or something else.

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 const userForm = document.getElementById("user-Form");
 
+	// Set a variable for counter, when each time a comment is submitted ++. When setting and item to local storage, up name by one(i.e. userInput1, userInput2) by adding userInput and the locally stored counter
+
 	userForm.addEventListener('submit', function(event) {
     	event.preventDefault();
 	const userInput = document.getElementById("user-Input").value;
 	const submit = document.getElementById("submitButton").value;
 
 	let userMessage = document.createElement('P');
-	let t  = document.createTextNode(userInput);
-	userMessage.appendChild(t);
+	let print  = document.createTextNode(userInput);
+	userMessage.appendChild(print);
     document.body.appendChild(userMessage);
 
+	// localStorage.setItem("userInput", userInput);
+
+
+    console.log(print);
 });
 
-// console.log(userInput);
+// console.log(userForm);
+
+
+// localStorage.setItem(print, document.createTextNode(userInput));
